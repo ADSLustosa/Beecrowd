d678b72058851d6f9c6770dcdd48f2f2a752598d
ADSLustosa <adslustosa@gmail.com>
2024-08-22 00:47:42 -0300

    Atualizado

---
+++ 1-Iniciante/Projeto1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Projeto1001 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println();
+        int A = scan.nextInt();
+        System.out.println();
+        int B = scan.nextInt();
+
+        int soma = A + B;
+        System.out.println("X = " + soma);
+
+    }
+}
---
+++ 1-Iniciante/Projeto1002.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Projeto1002 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            double π = 3.14159;
+            double raio = scan.nextDouble();
+            double area = π * raio * raio;
+
+            System.out.format("A=%.4f\n" , area );
+
+            
+            /* Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π. Apresentar a mensagem "A=". Utilize variáveis de dupla precisão (double) */ 
+    }
+}
